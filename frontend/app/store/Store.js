d4a8b2b9723b44d62c134c27cd8a0d3c39deac1a
mandar-saiyan87 <mdman2257@gmail.com>
2023-11-23 18:12:00 +0530

    store modified

--- frontend/app/store/Store.js
+++ frontend/app/store/Store.js
@@ -2,9 +2,7 @@ import { create } from "zustand";
 
 const userStore = create((set) => ({
   current_user: null,
-  redirect_to : false,
   setUser: (user) => set({ current_user: user }),
-  setRedirectTo: (value) => set({redirect_to: value})
 }))
 
 export default userStore
