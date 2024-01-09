8551e60394505c46aa24376d76a7c0568f559e05
mandar-saiyan87 <mdman2257@gmail.com>
2024-01-09 11:50:04 +0530

    navbar changed

--- frontend/app/HomePage/Navbar.jsx
+++ frontend/app/HomePage/Navbar.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Link from 'next/link'
 import Cookies from 'js-cookie';
 import userStore from '../store/Store';
@@ -7,9 +7,28 @@ import userStore from '../store/Store';
 
 const Navbar = () => {
 
+  const searchBar = useRef()
   const user = userStore((state) => state.current_user)
   const setUser = userStore((state) => state.setUser)
   const token = Cookies.get('token')
+  const [searchClick, setSearchclick] = useState(false)
+
+  const handleSearch = (event) => {
+    if (searchBar.current && !searchBar.current.contains(event.target)) {
+      setSearchclick(false)
+    }
+  }
+
+  useEffect(() => {
+    if (searchClick) {
+      document.addEventListener('click', handleSearch)
+    }
+    return () => {
+      document.removeEventListener('click', handleSearch)
+    }
+  }, [searchClick])
+
+
 
   useEffect(() => {
     if (user === null && token) {
@@ -24,33 +43,46 @@ const Navbar = () => {
   })
 
   return (
-    <div className='w-full max-w-[1920px] m-auto py-2'>
+    <div className='w-full max-w-[1920px] m-auto py-4 sm:py-2'>
       <header className='bg-white flex gap-x-2 items-center justify-between px-3 pt-3'>
         <Link href='/' className='flex items-center gap-1 text-primary'>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-7 h-7 -rotate-90 lg:w-8 lg:h-8">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-8 h-8 -rotate-90">
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
           </svg>
           <span className='hidden lg:block font-bold text-xl'>airbnc</span>
         </Link>
-        <div className='hidden sm:flex items-center rounded-full border-[1px] gap-2.5 shadow-md px-4 py-2 text-sm'>
-          <div className='cursor-pointer'>Anywhere</div>
-          <div className='bg-slate-200 w-[1px] h-5'></div>
-          <div className='cursor-pointer'>Any Week</div>
-          <div className='bg-slate-200 w-[1px] h-5'></div>
-          <div>Add Guests</div>
+        <div className='hidden sm:flex max-w-[334px] items-center rounded-full border-[1px] gap-2.5 shadow-md px-4 py-3 text-sm'>
+          <div className='w-full cursor-pointer text-center'>Anywhere</div>
+          <div className='bg-slate-200 w-[1px] h-6'></div>
+          <div className='w-full cursor-pointer text-center'>Any Week</div>
+          <div className='bg-slate-200 w-[1px] h-6'></div>
+          <div className='w-full text-center'>
+            <input type="text" placeholder='Add Guests' className='w-full outline-none placeholder:text-black' />
+          </div>
+          {/* <div>Add Guests</div> */}
           <button className='bg-primary flex items-center justify-center text-white p-1 rounded-full cursor-pointer'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </button>
         </div>
-        <div className='gap-x-1 max-h-max flex items-center rounded-full border-[1px] p-2 shadow-md text-sm sm:hidden'>
-          <button className='bg-primary flex items-center justify-center text-white p-1 rounded-full cursor-pointer'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+        {/* mobile/Tab Search bar */}
+        <div className='max-h-max flex items-center rounded-full border-[1px] p-2 shadow-md text-sm sm:hidden' ref={searchBar}>
+          <button className='bg-primary flex items-center justify-center text-white p-2 rounded-full cursor-pointer'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5">
               <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
           </button>
-          <input type="text" placeholder='anywhere, any week ...' className='focus:outline-none' />
+          <div className='flex items-center justify-center px-2' onClick={() => setSearchclick(true)}>
+            {searchClick ? <input type='text' className='outline-none' /> :
+              <div>
+                <p className='font-semibold'>Where to?</p>
+                <p className='text-[11px] text-slate-500 leading-normal'>Anywhere . Any Week . Add Guests</p>
+              </div>
+            }
+
+          </div>
+          {/* <input type="text" placeholder='Anywhere . Any Week . Add Guest' className='focus:outline-none' /> */}
         </div>
         <Link href={user ? '/account/profile' : '/auth/login'} className='flex cursor-pointer sm:px-3 py-2 md:border-[1px] gap-2.5 rounded-full items-center'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="hidden md:flex w-5 h-5">
